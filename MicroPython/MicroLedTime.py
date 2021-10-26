e25276c96e1f9d9f6b670344b24b8ad85ddd6668
Simon Lee <simon@leefamily.uk.net>
2021-10-26 18:28:00 +0100

    Create MicroLedTime.py

---
+++ MicroPython/MicroLedTime.py
@@ -0,0 +1,20 @@
+# Import Module
+from machine import Pin
+import time
+
+# Define LED
+led = Pin(11, Pin.OUT)
+
+# Define Button
+button = Pin(10, Pin.IN, Pin.PULL_DOWN)
+
+led.value(1) # 1 OFF 0 ON
+
+# Button Code
+while True:
+    if button.value() == 1:
+        led.value(0) # 1 OFF 0 ON
+        print("LED ON")
+    else:
+        led.value(1)
+    time.sleep(0.1)
